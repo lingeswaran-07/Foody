c54867784029bcaf6b316117fad9a3c187e4119c
LINGESWARAN V <lingeswaran.v2022ece@sece.ac.in>
2026-09-02 17:51:47 +0530

    'admin'

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Sidebar from './components/sidebar/Sidebar'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Orders from './pages/Orders/Orders'
-import Add from './pages/Add/Add.jsx'
-import List from './pages/List/List.jsx'
- import { ToastContainer } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
-const App = () => {
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Orders from "./pages/Orders/Orders";
+import Add from "./pages/Add/Add.jsx";
+import List from "./pages/List/List.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-  const url = "http://localhost:4000"
+const App = () => {
+  const url = "https://foody-backend-o94t.onrender.com";
   return (
     <div>
-      <ToastContainer/>
+      <ToastContainer />
       <Navbar />
       <hr />
       <div className="app-content">
-       <Sidebar />
-       <Routes>
-        <Route path="/add" element={<Add url={url}/>}/>
-        <Route path="/list" element={<List url={url}/>}/>
-        <Route path="/orders" element={<Orders url={url}/>}/>
-       </Routes>
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
+        </Routes>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
